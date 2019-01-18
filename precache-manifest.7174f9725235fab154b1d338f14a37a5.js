2236b051e5e74dae499790891f473e663f5b05af
Balazs Kosa <b4l4g3@gmail.com>
2019-01-18 15:13:57 +0100

    Updates

--- precache-manifest.7174f9725235fab154b1d338f14a37a5.js
+++ precache-manifest.7174f9725235fab154b1d338f14a37a5.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/music-quiz-app/static/js/runtime~main.633bc8d0.js"
   },
   {
-    "revision": "1d89ab16231e6c5225dc",
-    "url": "/music-quiz-app/static/js/main.1d89ab16.chunk.js"
+    "revision": "0d259f9716689622d25d",
+    "url": "/music-quiz-app/static/js/main.0d259f97.chunk.js"
   },
   {
     "revision": "de49f17edaed8fff69e3",
     "url": "/music-quiz-app/static/js/1.de49f17e.chunk.js"
   },
   {
-    "revision": "c824cc2a653c15d076140072a57dca73",
+    "revision": "26bfed05d3489c74fa5ac633b0bb9bd9",
     "url": "/music-quiz-app/index.html"
   }
 ];
